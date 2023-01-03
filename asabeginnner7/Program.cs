af10f2dd15492d46cfd6b519a9cde0e37f030f3f
iampr <iampr@PRABIN>
2023-01-03 22:49:45 +0545

    Add project files.

---
+++ asabeginnner7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        //implicit converter as it dont lose any data 
+        //as float is bigger data type 
+        int i = 1010;
+        float f = i;
+        Console.WriteLine(f);
+    }
+}
